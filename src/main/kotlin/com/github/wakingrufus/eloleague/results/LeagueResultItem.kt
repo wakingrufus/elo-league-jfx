124f41925c04509aa489bf27ad41d27c677bb5da
John Burns <wakingrufus@gmail.com>
2018-06-18 17:49:49 -0500

    add button to results view to apply filter pass league trial period into the results to use as filter threshold upgrade to kotlin 1.2.31
    update to version 0.9.0
    
    Signed-off-by: John Burns <wakingrufus@gmail.com>

--- src/main/kotlin/com/github/wakingrufus/eloleague/results/LeagueResultItem.kt
+++ src/main/kotlin/com/github/wakingrufus/eloleague/results/LeagueResultItem.kt
@@ -2,13 +2,12 @@ package com.github.wakingrufus.eloleague.results
 
 import javafx.beans.property.SimpleListProperty
 import javafx.collections.FXCollections
-import tornadofx.getValue
-import tornadofx.setValue
+import tornadofx.*
 
 class LeagueResultItem {
     val playersProperty = SimpleListProperty<PlayerResultItem>(this, "players", FXCollections.observableArrayList())
     var players by playersProperty
 
     val gamesProperty = SimpleListProperty<GameResultItem>(this, "games", FXCollections.observableArrayList())
-   var games by gamesProperty
+    var games by gamesProperty
 }
--- src/test/kotlin/com/github/wakingrufus/eloleague/results/ResultsViewTest.kt
+++ src/test/kotlin/com/github/wakingrufus/eloleague/results/ResultsViewTest.kt
@@ -1,6 +1,7 @@
 package com.github.wakingrufus.eloleague.results
 
 import com.github.wakingrufus.eloleague.TornadoFxTest
+import javafx.scene.control.TableView
 import mu.KLogging
 import org.junit.Test
 import org.testfx.api.FxAssert
@@ -11,12 +12,20 @@ class ResultsViewTest : TornadoFxTest() {
 
     @Test
     fun test() {
-        val results: LeagueResultItem = LeagueResultItem()
+        val results: LeagueResultItem = LeagueResultItem().apply {
+            players.add(PlayerResultItem(id = "1", name = "1", gamesPlayed = 0, losses = 0, wins = 0, currentRating = 1500))
+            players.add(PlayerResultItem(id = "2", name = "2", gamesPlayed = 1, losses = 1, wins = 0, currentRating = 1490))
+            players.add(PlayerResultItem(id = "3", name = "3", gamesPlayed = 2, losses = 1, wins = 1, currentRating = 1501))
+        }
         showViewWithParams<ResultsView>(mapOf(
+                "trialPeriod" to 1,
                 "leagueResultItem" to results
         ))
 
         FxAssert.verifyThat("#results-wrapper", NodeMatchers.isVisible())
+        FxAssert.verifyThat<TableView<PlayerResultItem>>("#results-tableview", { it.items.size == 3 })
+        clickOn("#filter-newbies-button")
+        FxAssert.verifyThat<TableView<PlayerResultItem>>("#results-tableview", { it.items.size == 2 })
     }
 
 }
